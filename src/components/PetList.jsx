dad5f6bdbe2dc6895b5af79ddfd5b44c4eb6903b
arpita-comeco <arpita.behura@comeco.com>
2022-02-15 17:47:28 +0100

    Making React components observable

--- src/components/PetList.jsx
+++ src/components/PetList.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { observer } from "mobx-react-lite";
 
 function PetList({ store }) {
-  return <div>{store.storeDetails}</div>;
+    const handleAddPet=(e) =>{
+        const name = prompt("Name of the pet");
+        const type = prompt("Type of the pet");
+        const breed = prompt("Breed of the pet");
+        const ownerId = prompt("Owner's Id of the pet");
+
+        const pet = store.createPet({ id: Date.now(), name, breed, type });
+        store.assignOwnerToPet(ownerId, pet.id);
+    }
+    return (
+        <div>
+            {store.storeDetails}
+            <br/>
+            <button onClick={handleAddPet}>Add new Pet</button>
+        
+        </div>
+  );
 }
 
-export default PetList;
+export default observer(PetList);
